ba5a710db1a2ae3cb7dd131b14df3742f8a2e84f
Alejandro Aragon <aragondru155@gmail.com>
2023-07-09 16:26:42 -0600

    by id function

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CountriesService } from '../../service/countries.service';
+import { switchMap } from 'rxjs';
+import { Country } from '../../interface/countries.interface';
 
 @Component({
   selector: 'app-country-page',
@@ -6,6 +10,30 @@ import { Component } from '@angular/core';
   styles: [
   ]
 })
-export class CountryPageComponent {
+export class CountryPageComponent implements OnInit{
+
+  public country?:Country;
+
+  constructor(
+    private activedRoute:ActivatedRoute,
+    private countriesService:CountriesService,
+    private router:Router
+  ) {}
+  
+  
+  ngOnInit(): void {
+    this.activedRoute.params.
+                      pipe(
+                        switchMap(({id}) => this.countriesService.searchCountryByAlphaCode(id))
+                      )
+                      .subscribe(country =>{
+                        if(!country)
+                          return this.router.navigateByUrl('')
+
+                        
+                        return this.country=country;
+                        
+                      });
+  }
 
 }
